901b8b68817b759f68b4d2ccdc7bc83095606d27
K-kare <kamunawangari@gmail.com>
2024-02-05 02:56:27 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """define that inherits from list"""
 
+
 class MyList(list):
     """executes the list, but sorted ascending sort"""
+
     def print_sorted(self):
         """prints the list, but sorted ascending sort"""
         print (sorted(self))
